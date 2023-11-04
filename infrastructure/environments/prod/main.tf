0184c72f82db0986da2a77b4d159d239ca953e32
Alexander Potts <ajpotts01@gmail.com>
2023-11-04 13:50:30 +1100

    Fix modules path

--- infrastructure/environments/prod/main.tf
+++ infrastructure/environments/prod/main.tf
@@ -1,5 +1,5 @@
 module "service_account" {
-  source     = "../../service_account"
+  source     = "../../modules/service_account"
   project_id = var.project_id
   env        = var.env
   app_name   = var.app_name
