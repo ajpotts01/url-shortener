fc1d99d72efd77c6419a16663557eac31026dad5
Alexander Potts <ajpotts01@gmail.com>
2023-11-05 23:31:13 +1100

    Current progress - needs db, tests

--- application/api/api_urls.go
+++ application/api/api_urls.go
@@ -1,11 +1,45 @@
 package api
 
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+
+	"github.com/ajpotts01/url-shortener/application/internal/urls"
+)
+
 type urlRequest struct {
 	Url string `json:"url"`
 }
 
-func (config *ApiConfig) CreateShortenedUrl(url urlRequest) (Url, error) {
-	return Url{}, nil
+func (config *ApiConfig) CreateShortenedUrl(w http.ResponseWriter, r *http.Request) {
+	decoder := json.NewDecoder(r.Body)
+	requestParams := urlRequest{}
+	err := decoder.Decode(&requestParams)
+
+	if err != nil {
+		log.Printf("Error decoding parameters: %s", err)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+
+	// TODO: Handle collisions
+	urlShort, err := urls.GetShortenedUrl(requestParams.Url)
+
+	if err != nil {
+		log.Printf("Error creating new shortened URL: %v", err)
+		errorResponse(w, http.StatusInternalServerError, err.Error())
+		return
+	}
+
+	// TODO: Save to DB
+
+	validResponse(w, http.StatusCreated, Url{
+		UrlLong:  requestParams.Url,
+		UrlShort: urlShort,
+	})
 }
 
 func (config *ApiConfig) FetchShortenedUrl(url urlRequest) (Url, error) {
